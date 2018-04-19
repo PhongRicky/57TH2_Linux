c4fff2c1d3271a57232f71816a5062fae3f3de27
VuHungPhong <38232612+PhongRicky@users.noreply.github.com>
2018-04-19 21:58:56 +0700

    The first

---
+++ sohoc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+a= input("a= ")
+b= input("b= ")
+print'%d + %d = %d'%(a,b,a+b)
+print'%d - %d = %d'%(a,b,a-b)
+print'%d * %d = %d'%(a,b,a*b)
+print'%d / %d = %f'%(a,b,float(a)/b)
+#print'%d % %d = %.2f'%(a,b,float(a)/b)
+#57TH2_Linux
