582c4746f698ead4d6b0693f231d633af682f3e9
VuHungPhong <38232612+PhongRicky@users.noreply.github.com>
2018-04-24 20:33:10 +0700

    Update OPP.py

--- OPP.py
+++ OPP.py
@@ -4,36 +4,44 @@ class SinhVien:
 		self.maSV = maSV
 		self.hoTen = hoTen
 		self.maKhoa = maKhoa
+
 	def printSV(self):
 		print self.maSV, '\t', self.hoTen, '\t', self.maKhoa
+
+	def get_maKhoa(seft):
+		return seft.maKhoa
 
 class Khoa:
 	def __init__(self,maKhoa,tenKhoa):
 		self.maKhoa = maKhoa
 		self.tenKhoa = tenKhoa
+
 	def printKhoa(self):
 		print self.maKhoa, '\t', self.tenKhoa
+	
 
-
-sv1= SinhVien('001','Mai A','01')
-sv2= SinhVien('002','Tran B','01')
-sv3= SinhVien('003','Van C','02')
-sv4= SinhVien('004','Thi K','001') 
-
-k1= Khoa('01','CNTT')
-k2= Khoa('02','KToan')
-k3= Khoa('03','Co Khi')
-k4= Khoa('04','Nuoi')
-
+sv=[]
+sv.append(SinhVien('001','Mai A','01'))
+sv.append(SinhVien('002','Tran B','01'))
+sv.append(SinhVien('003','Van C','02'))
+sv.append(SinhVien('004','Thi K','01'))
 print 'MSSV\tHo ten\tMa khoa'
-sv1.printSV()
-sv2.printSV()
-sv3.printSV()
-sv4.printSV()
+for i in sv:
+	i.printSV()
+
+k=[]
+k.append(Khoa('01','CNTT'))
+k.append(Khoa('02','KToan'))
+k.append(Khoa('03','Co Khi'))
+k.append(Khoa('04','Nuoi'))
 print'------------------------'
 print 'Ma khoa\tTen khoa'
-k1.printKhoa()
-k2.printKhoa()
-k3.printKhoa()
-k4.printKhoa()
+for j in k:
+	j.printKhoa()
 
+print'------------------------'
+print 'SV thuoc khoa CNTT'
+print 'MSSV\tHo ten\tMa khoa'
+for h in sv:
+	if(h.get_maKhoa()=='01'):
+		h.printSV()
