a2b282d7cc120bc83b1552edd4b1c812c223fe02
VuHungPhong <38232612+PhongRicky@users.noreply.github.com>
2018-04-26 21:57:30 +0700

    Shell_bai4

---
+++ bai4.sh
@@ -0,0 +1,22 @@
+#bai 5
+clear
+echo "--------Menu--------"
+echo "1. Cafe den"
+echo "2. Cafe da"
+echo "3. Cafe sua"
+echo "4. Cafe cao"
+
+read -p "Ban chon thuc uong nao (1-4)?: " chon
+case $chon in
+1) 
+	echo "Ban da chon Cafe den";;
+2)
+	echo "Ban da chon Cafe da";;
+3)
+	echo "Ban da chon Cafe sua";;
+4)
+	echo "Ban da chon Cafe cao"
+	exit;;
+esac 
+	
+
