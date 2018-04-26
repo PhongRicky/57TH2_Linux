4f4e375987ac51cc25d243445fd94f549e8dfd87
VuHungPhong <38232612+PhongRicky@users.noreply.github.com>
2018-04-26 21:55:23 +0700

    Shell_bai1

---
+++ bai1.sh
@@ -0,0 +1,14 @@
+#Bai 1
+clear
+
+echo "Hello phongvu" 
+
+echo "------------Thu muc hien hanh------------"
+pwd 
+
+
+echo "------------Liet ke tap tin va thu muc----------"
+ls -a 
+
+echo "------------Ngay gio hien tai-----------"
+date 
