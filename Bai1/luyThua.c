f69fb7498b56f9be81cf14ad66e8c42e0faeb072
VuHungPhong <38232612+PhongRicky@users.noreply.github.com>
2018-05-17 10:04:42 +0700

    Update luyThua.c

--- Bai1/luyThua.c
+++ Bai1/luyThua.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-#include<math.h>
 
-int luyThua(int x, int n)
-{
-	return pow(x,n);
+int luythua(int x, int n)
+{	int S=1;
+	for(int i=1; i<=n; i++)
+	{
+		S*=x;
+	}
+	return S;
 }
